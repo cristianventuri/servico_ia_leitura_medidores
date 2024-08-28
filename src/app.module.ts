885d421133032c69cd11b64468bfc1fbc002256e
cristianventuri <crisventuri@hotmail.com>
2024-08-28 20:08:33 -0300

    feat: adicionado configuração do sequelize  para o banco de dados

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,23 @@
 import { Module } from '@nestjs/common';
+import { SequelizeModule } from '@nestjs/sequelize';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MeasureModule } from './measure/measure.module';
 
 @Module({
-  imports: [],
+  imports: [
+    SequelizeModule.forRoot({
+      dialect: 'postgres',
+      host: 'postgres',
+      port: 5432,
+      username: 'postgres',
+      password: 'postgres',
+      database: 'shopper',
+      autoLoadModels: true,
+      synchronize: true,
+    }),
+    MeasureModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
