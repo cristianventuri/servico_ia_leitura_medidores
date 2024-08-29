adfc1cd18a7033cbdc925be5bf42c87c8db636e9
cristianventuri <crisventuri@hotmail.com>
2024-08-29 18:49:35 -0300

    refactor: ajustado tamnaho máximo permitido na request e liberação de diretório public para disponibilizar imagens

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,26 @@
 import { NestFactory } from '@nestjs/core';
+import * as express from 'express';
+import { existsSync, mkdirSync } from 'fs';
+import * as path from 'path';
 import { AppModule } from './app.module';
+
+export const PUBLIC_DIR = path.join(__dirname, '..', 'public');
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  /* Ajusta tamanho máximo permitido */
+  app.use(express.json({ limit: '100mb' }));
+  app.use(express.urlencoded({ limit: '100mb', extended: true }));
+
+  const existsDir = await existsSync(PUBLIC_DIR);
+  if (!existsDir) {
+    await mkdirSync(PUBLIC_DIR, { recursive: true });
+  }
+
+  /* Disponibiliza diretório público*/
+  app.use('/public', express.static(PUBLIC_DIR));
+
+  await app.listen(8080);
 }
 bootstrap();
