f3c84d7c5c4fcaf676f97d1c86ee7c0282dc98b9
cristianventuri <crisventuri@hotmail.com>
2024-08-29 23:25:56 -0300

    feat: registrado novos services ao módulo

--- src/measure/measure.module.ts
+++ src/measure/measure.module.ts
@@ -2,10 +2,23 @@ import { Module } from '@nestjs/common';
 import { SequelizeModule } from '@nestjs/sequelize';
 import { Measure } from './measure.model';
 import { MeasureRepository } from './measure.repository';
+import { MeasureConfirmService } from './measureConfirm.service';
+import { MeasureListService } from './measureList.service';
+import { MeasureUploadService } from './measureUpload.service';
 
 @Module({
   imports: [SequelizeModule.forFeature([Measure])],
-  providers: [MeasureRepository],
-  exports: [MeasureRepository],
+  providers: [
+    MeasureUploadService,
+    MeasureListService,
+    MeasureConfirmService,
+    MeasureRepository,
+  ],
+  exports: [
+    MeasureUploadService,
+    MeasureListService,
+    MeasureConfirmService,
+    MeasureRepository,
+  ],
 })
 export class MeasureModule {}
