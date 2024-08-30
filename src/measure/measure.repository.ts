e1d1408cd4a36d105484b0de6f17f2e4b36f4abd
cristianventuri <crisventuri@hotmail.com>
2024-08-29 23:25:31 -0300

    refactor: ajustado repository para persistencia de dados

--- src/measure/measure.repository.ts
+++ src/measure/measure.repository.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/sequelize';
+import { Op, Sequelize } from 'sequelize';
+import { ParamList, QueryList } from 'src/dto/list.dto';
 import { Measure } from './measure.model';
 
 @Injectable()
@@ -9,33 +11,62 @@ export class MeasureRepository {
     private readonly measureModel: typeof Measure,
   ) {}
 
-  async create(data: Partial<Measure>): Promise<Measure> {
+  /**
+   * Realiza a inserção de novos registros no banco de dados.
+   *
+   * @param { Partial<Measure>} data
+   * @returns {Promise<Measure>}
+   */
+  public async create(data: Partial<Measure>): Promise<Measure> {
     return this.measureModel.create(data);
   }
 
-  async findAll(): Promise<Measure[]> {
-    return this.measureModel.findAll();
+  /**
+   * Verifica a existência de determinada medição.
+   *
+   * @param {Partial<Measure>} data
+   * @returns {Promise<Measure>}
+   */
+  public async checkExistingMeasure(data: Partial<Measure>): Promise<Measure> {
+    return this.measureModel.findOne({
+      where: {
+        customer_code: data.customer_code,
+        measure_type: data.measure_type,
+        measure_datetime: {
+          [Op.between]: [
+            Sequelize.literal("DATE_TRUNC('month', NOW())"),
+            Sequelize.literal(
+              "DATE_TRUNC('month', NOW()) + INTERVAL '1 month - 1 second'",
+            ),
+          ],
+        },
+      },
+    });
   }
 
-  async findById(id: number): Promise<Measure> {
-    return this.measureModel.findOne({ where: { id } });
+  /**
+   * Realiza a busca da medição com base no UUID
+   *
+   * @param measure_uuid
+   * @returns {Promise<Measure>}
+   */
+  public async findByUuid(measure_uuid: string): Promise<Measure> {
+    return this.measureModel.findOne({ where: { measure_uuid } });
   }
 
-  async findByGuid(guid: string): Promise<Measure> {
-    return this.measureModel.findOne({ where: { guid } });
-  }
-
-  async update(
-    id: number,
-    data: Partial<Measure>,
-  ): Promise<[number, Measure[]]> {
-    return this.measureModel.update(data, { where: { id }, returning: true });
-  }
-
-  async delete(id: number): Promise<void> {
-    const measure = await this.findById(id);
-    if (measure) {
-      await measure.destroy();
-    }
+  /**
+   * Realiza a busca de todas as medições com base no data
+   *
+   * @param measure_uuid
+   * @returns {Promise<Measure[]>}
+   */
+  public async findAll(
+    data: Partial<ParamList & QueryList>,
+  ): Promise<Measure[]> {
+    return this.measureModel.findAll({
+      where: {
+        ...data,
+      },
+    });
   }
 }
